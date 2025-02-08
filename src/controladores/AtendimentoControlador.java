6057ce13b47a8f1b4fe3e291bb6b6cec4d4ad7f5
LuizFarias21 <luizbfarias21@gmail.com>
2025-02-08 13:19:30 -0300

    Atualizado AtendimentoControlador

--- src/controladores/AtendimentoControlador.java
+++ src/controladores/AtendimentoControlador.java
@@ -1,4 +1,40 @@
 package controladores;
 
+import entidades.Consulta;
+import entidades.Medico;
+import entidades.Paciente;
+import entidades.Prescricao;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
 public class AtendimentoControlador {
+
+
+//    public void agendarConsulta(
+//            LocalDate dataAgendada,
+//            LocalTime horarioInicialConsulta,
+//            LocalTime duracaoConsulta,
+//            Paciente pacienteAssociado,
+//            Medico medicoResponsavel,
+//            Prescricao prescricao,
+//            String especialidadeRequerida,
+//            double valor) {
+//
+//        Consulta novaConsulta = new Consulta(dataAgendada, horarioInicialConsulta, duracaoConsulta, Consulta.Status.AGENDADA, pacienteAssociado, medicoResponsavel, prescricao, especialidadeRequerida, valor);
+//
+//        if (pacienteAssociado == null || medicoResponsavel == null || especialidadeRequerida == null) {
+//            throw new IllegalArgumentException("Erro! Você precisa inserir todos os dados corretamente!");
+//        }
+//        consultaServico.agendarConsulta(novaConsulta);
+//    }
+//
+//    public void cancelarConsulta(Consulta consulta) {
+//
+//        if (consulta == null) {
+//            System.out.println("Não existe essa consulta, portanto não pode ser cancelada!");
+//            return;
+//        }
+//        consultaServico.cancelarConsulta(consulta);
+//    }
 }
--- src/controladores/ConsultaControlador.java
+++ src/controladores/ConsultaControlador.java
@@ -16,30 +16,4 @@ public class ConsultaControlador {
         this.consultaServico = consultaServico;
     }
 
-    public void agendarConsulta(
-                                LocalDate dataAgendada,
-                                LocalTime horarioInicialConsulta,
-                                LocalTime duracaoConsulta,
-                                Paciente pacienteAssociado,
-                                Medico medicoResponsavel,
-                                Prescricao prescricao,
-                                String especialidadeRequerida,
-                                double valor) {
-
-        Consulta novaConsulta = new Consulta(dataAgendada, horarioInicialConsulta, duracaoConsulta, Consulta.Status.AGENDADA, pacienteAssociado, medicoResponsavel, prescricao, especialidadeRequerida, valor);
-
-        if (pacienteAssociado == null || medicoResponsavel == null || especialidadeRequerida == null) {
-            throw new IllegalArgumentException("Erro! Você precisa inserir todos os dados corretamente!");
-        }
-        consultaServico.agendarConsulta(novaConsulta);
-    }
-
-    public void cancelarConsulta(Consulta consulta) {
-
-        if (consulta == null) {
-            System.out.println("Não existe essa consulta, portanto não pode ser cancelada!");
-            return;
-        }
-        consultaServico.cancelarConsulta(consulta);
-    }
 }
