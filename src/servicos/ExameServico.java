2dfab4c8967726e1176125f761d2d084c90d9d06
LuizFarias21 <luizbfarias21@gmail.com>
2025-02-09 23:33:29 -0300

    Revertendo commit de João Guilherme
    This reverts commit 28ed36a5683b2e2d153d310cc7ac15faf2c32516, reversing
    changes made to b0bbf14106e570a95e9c3a1515cfe3921a7301a7.

--- src/servicos/ExameServico.java
+++ src/servicos/ExameServico.java
@@ -1,17 +1,7 @@
 package servicos;
 
 import entidades.Exame;
-import entidades.Consulta;
-import repositorios.ExameRepositorio;
 
 public class ExameServico extends ClinicaServico<Exame> {
 
-    public ExameServico(ExameRepositorio exameRepositorio) {
-        super(exameRepositorio);
-    }
-
-    public void associarExameAConsulta(Exame exame, Consulta consulta) {
-        consulta.getExamesRealizados().add(exame);
-        exame.setConsultaAssociada(consulta);
-    }
 }
--- src/visoes/MedicamentoVisao.java
+++ src/visoes/MedicamentoVisao.java
@@ -1,77 +1,33 @@
 package visoes;
 
-import javax.swing.*;
-import controladores.MedicamentoControlador;
-import entidades.Medicamento;
-import excecoes.DadoInvalidoException;
-
 public class MedicamentoVisao extends GenericoVisao {
+    @Override
+    public void exibirVisao() {
 
-    private MedicamentoControlador medicamentoControlador;
-
-    public MedicamentoVisao(MedicamentoControlador medicamentoControlador) {
-        this.medicamentoControlador = medicamentoControlador;
     }
 
     @Override
-    public void exibirVisao() throws DadoInvalidoException {
-        while (true) {
-            String[] opcoes = {"Cadastrar Medicamento", "Buscar Medicamento", "Listar Medicamentos",
-                    "Remover Medicamento", "Voltar"};
+    public void cadastrarVisao() {
 
-            int escolha = JOptionPane.showOptionDialog(null, "Menu Medicamentos", "Escolha uma opção",
-                    JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE, null, opcoes, opcoes[0]);
-
-            switch (escolha) {
-                case 0: // Cadastrar medicamento
-                    cadastrarVisao();
-                    break;
-                case 1: // Buscar medicamento
-                    buscarVisao();
-                    break;
-                case 2: // Listar medicamentos
-                    listarVisao();
-                    break;
-                case 3: // Remover medicamento
-                    excluirVisao();
-                    break;
-                case 4: // Sair
-                    return;
-            }
-        }
     }
 
     @Override
-    public void cadastrarVisao() throws DadoInvalidoException {
-        String nome = JOptionPane.showInputDialog("Digite o nome do medicamento:");
-        String tipo = JOptionPane.showInputDialog("Digite o tipo do medicamento (ex: comprimido, solução):");
-        String dosagem = JOptionPane.showInputDialog("Digite a dosagem do medicamento:");
-        String descricao = JOptionPane.showInputDialog("Digite a descrição do medicamento:");
+    public void buscarVisao() {
 
-        Medicamento medicamento = new Medicamento(nome);
-
-        medicamentoControlador.cadastrar(medicamento);
     }
 
     @Override
-    public void buscarVisao() throws DadoInvalidoException {
-        String nome = JOptionPane.showInputDialog("Digite o nome do medicamento:");
-        medicamentoControlador.buscar(nome);
-    }
+    public void listarVisao() {
 
-    @Override
-    public void listarVisao() throws DadoInvalidoException {
-        medicamentoControlador.listar();
     }
 
     @Override
     public void atualizarVisao() {
-        JOptionPane.showMessageDialog(null, "Funcionalidade ainda não implementada.");
+
     }
 
     @Override
-    public void excluirVisao() throws DadoInvalidoException {
-        String nome = JOptionPane.showInputDialog("Digite o nome do medicamento que deseja excluir:");
-        medicamentoControlador.excluir(nome);
+    public void excluirVisao() {
+
     }
 }
